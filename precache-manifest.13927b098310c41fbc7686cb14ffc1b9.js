b27338a53d8beb562377687cddc5e48027e88b35
thanhtran1324 <thanhtran1324@gmail.com>
2020-01-27 13:53:18 -0500

    Updates

---
+++ precache-manifest.13927b098310c41fbc7686cb14ffc1b9.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a388d0e9bde23fa12db7c63a11ccbf73",
+    "url": "/index.html"
+  },
+  {
+    "revision": "193fe638b9067c6ef34a",
+    "url": "/static/js/2.17e1c65a.chunk.js"
+  },
+  {
+    "revision": "82089ce1cf768ecb1d68",
+    "url": "/static/js/main.e0620703.chunk.js"
+  },
+  {
+    "revision": "1337678ae3301a2c8a7b",
+    "url": "/static/js/runtime-main.8eac158a.js"
+  }
+]);
