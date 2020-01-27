47202e8eed2130402f1d23e8ae8d28cc42f33a32
thanhtran1324 <thanhtran1324@gmail.com>
2020-01-27 14:08:11 -0500

    Updates

---
+++ precache-manifest.b237668b951b729b815aac47bedc67a1.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d8cc51e1087281118190cfb739a2ca0d",
+    "url": "/shoppingcart/index.html"
+  },
+  {
+    "revision": "1e9c15e5ce71ab626a19",
+    "url": "/shoppingcart/static/js/2.f80098a5.chunk.js"
+  },
+  {
+    "revision": "9f84168fa9d3d549090c",
+    "url": "/shoppingcart/static/js/main.d54999ec.chunk.js"
+  },
+  {
+    "revision": "c13d74c0ab21fc77152b",
+    "url": "/shoppingcart/static/js/runtime-main.bd6453df.js"
+  }
+]);
