4114a86e7b08391f3572b9da01966eb677acd377
thanhtran1324 <thanhtran1324@gmail.com>
2020-05-18 11:02:56 -0400

    Updates

---
+++ precache-manifest.3a610d2859dbd2afcafa7f81374d0253.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e03f2566b4c1380e78c51d179448fd18",
+    "url": "/shoppingcart/index.html"
+  },
+  {
+    "revision": "44a77a364557b40b0eca",
+    "url": "/shoppingcart/static/css/2.e5677a71.chunk.css"
+  },
+  {
+    "revision": "fa088eaffc6b492ed161",
+    "url": "/shoppingcart/static/css/main.b1a1f876.chunk.css"
+  },
+  {
+    "revision": "44a77a364557b40b0eca",
+    "url": "/shoppingcart/static/js/2.d02dae89.chunk.js"
+  },
+  {
+    "revision": "622b768fb0c3812591e65cb93882ca79",
+    "url": "/shoppingcart/static/js/2.d02dae89.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "fa088eaffc6b492ed161",
+    "url": "/shoppingcart/static/js/main.a543e327.chunk.js"
+  },
+  {
+    "revision": "667b3aa3eb2aca92e739",
+    "url": "/shoppingcart/static/js/runtime-main.dbd434ae.js"
+  },
+  {
+    "revision": "0b4ac1dc75df35e169b70d7719afe4cc",
+    "url": "/shoppingcart/static/media/notification.0b4ac1dc.ttf"
+  },
+  {
+    "revision": "5bee74caefdf9d0a834915f6c8eeb259",
+    "url": "/shoppingcart/static/media/notification.5bee74ca.svg"
+  },
+  {
+    "revision": "651771e1df95c807c99608188d0a4287",
+    "url": "/shoppingcart/static/media/notification.651771e1.woff"
+  },
+  {
+    "revision": "c0d3c94cd6112550c51d7d1ed13b9da1",
+    "url": "/shoppingcart/static/media/notification.c0d3c94c.eot"
+  },
+  {
+    "revision": "f9b63344bd941ac0793c0581144d9e2d",
+    "url": "/shoppingcart/static/media/thanh_logo.f9b63344.jpg"
+  }
+]);
